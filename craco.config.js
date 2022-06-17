cce95d0db6b8c9a1162179212acb8bf8b87c60b5
cycyewha054 <sbn04134@ewhain.net>
2022-06-17 23:23:29 +0900

    build

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 module.exports = {
   style: {
     postcssOptions: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      paths.appBuild = webpackConfig.output.path = path.resolve('docs');
+      return webpackConfig; // Important: return the modified config
     },
   },
 };
